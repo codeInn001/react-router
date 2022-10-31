f4ab3c0ecbd913ac244f377e74bc19b1c8578eac
codeInn001 <emmaalege@gmail.com>
2022-10-31 23:59:34 +0100

    updated error404

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,6 +5,7 @@ const PageNotFound = () => {
         <div id="wrapper">
             <img src="https://i.imgur.com/qIufhof.png" />
             <div id="info">
+                <h2>Error 404</h2>
                 <h3>This page could not be found</h3>
             </div>
         </div >
